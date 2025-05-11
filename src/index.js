1b093aaac7e3cd0c1c96e22a0cf2a6b5de1273c0
Deba29 <debajyoti@rhythmflows.com>
2025-05-11 15:09:30 +0530

    route

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/debajyoti-maity-portfolio">
       <App />
     </Router>
   </React.StrictMode>
